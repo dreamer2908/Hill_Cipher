5b12213a161c5a89719f795b5a304d29672bb2e9
dreamer2908 <dreamer2908@gmail.com>
2014-09-25 10:34:02 +0700

    Moar code

--- Form1.cs
+++ Form1.cs
@@ -14,5 +14,130 @@ namespace Hill_Cipher
         {
             InitializeComponent();
         }
+
+        private void updateKeyNum()
+        {
+            char key1 = (char)(numKey00.Value + 65);
+            char key2 = (char)(numKey01.Value + 65);
+            char key3 = (char)(numKey10.Value + 65);
+            char key4 = (char)(numKey11.Value + 65);
+            txtKey.Text = key1.ToString() + key2.ToString() + key3.ToString() + key4.ToString();
+        }
+
+        private void updateKeyString()
+        {
+            // remove non alpha chars
+            string newKey = txtKey.Text.ToUpper();
+            string key = "";
+            for (int i = 0; i < txtKey.Text.ToUpper().Length; i++)
+            {
+                char disChar = newKey[i];
+                if ((int)disChar <= (int)'Z' && ((int)disChar >= (int)'A'))
+                {
+                    key += disChar;
+                }
+            }
+            // append A if key is not long enough
+            while (key.Length < 4)
+            {
+                key += "A";
+            }
+            // trim key to 4
+            key = key.Substring(0, 4);
+            // write back values to txtKey and numKeyxy
+            txtKey.Text = key;
+            numKey00.Value = (int)key[0] - (int)'A';
+            numKey01.Value = (int)key[1] - (int)'A';
+            numKey10.Value = (int)key[2] - (int)'A';
+            numKey11.Value = (int)key[3] - (int)'A';
+        }
+
+        private void generateNewKey()
+        {
+            // TODO: find a convinient way to generate a good an usable key
+        }
+
+        private void encryptText()
+        {
+            // TODO: transfer it from python script to here
+        }
+
+        private void decryptText()
+        {
+            // TODO: transfer it from python script to here
+        }
+
+        private void numKey00_ValueChanged(object sender, EventArgs e)
+        {
+            updateKeyNum();
+        }
+
+        private void numKey01_ValueChanged(object sender, EventArgs e)
+        {
+            updateKeyNum();
+        }
+
+        private void numKey10_ValueChanged(object sender, EventArgs e)
+        {
+            updateKeyNum();
+        }
+
+        private void numKey11_ValueChanged(object sender, EventArgs e)
+        {
+            updateKeyNum();
+        }
+
+        private void rbtnKeySelect_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnKeySelect.Checked)
+            {
+                txtKey.ReadOnly = true;
+                numKey00.ReadOnly = false;
+                numKey01.ReadOnly = false;
+                numKey10.ReadOnly = false;
+                numKey11.ReadOnly = false;
+            }
+            else
+            {
+                txtKey.ReadOnly = false;
+                numKey00.ReadOnly = true;
+                numKey01.ReadOnly = true;
+                numKey10.ReadOnly = true;
+                numKey11.ReadOnly = true;
+            }
+        }
+
+        private void txtKey_Leave(object sender, EventArgs e)
+        {
+            updateKeyString();
+        }
+
+        private void btnCheckKey_Click(object sender, EventArgs e)
+        {
+            int det = (int)(numKey00.Value * numKey11.Value - numKey01.Value * numKey10.Value);
+            if (det != 0 && det % 2 != 0 && det % 13 != 0)
+            {
+                MessageBox.Show("This key is usable!", "Check key", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("This key is NOT usable! Please use \"New key\" function to generate a good key!","Check key", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            encryptText();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            decryptText();
+        }
+
+        private void btnGenKey_Click(object sender, EventArgs e)
+        {
+            generateNewKey();
+        }
     }
 }
---
+++ Matrix.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColorMatrixConverter
+{
+    public class Matrix
+    {
+        private readonly int[,] _matrix;
+
+        public Matrix(int dim1, int dim2)
+        {
+            _matrix = new int[dim1, dim2];
+        }
+
+        public int Height { get { return _matrix.GetLength(0); } }
+        public int Width { get { return _matrix.GetLength(1); } }
+
+        public int this[int x, int y]
+        {
+            get { return _matrix[x, y]; }
+            set { _matrix[x, y] = value; }
+        }
+
+        // modular multiplicative inverse
+        public static int modInverse(int _a, int m)
+        {
+            int a = _a % m;
+            for (int x = 0; x < m; x++)
+            {
+                if ((a * x) % m == 1) // according to the defination
+                {
+                    return x;
+                }
+            }
+            return -1; // couldn't find its modular multiplicative inverse
+        }
+
+        public static Matrix Multiplication(Matrix m1, Matrix m2)
+        {
+            Matrix resultMatrix = new Matrix(m1.Height, m2.Width);
+
+            for (int i = 0; i < resultMatrix.Height; i++)
+            {
+                for (int j = 0; j < resultMatrix.Width; j++)
+                {
+                    resultMatrix[i, j] = 0;
+                    for (int k = 0; k < m1.Width; k++)
+                    {
+                        resultMatrix[i, j] += m1[i, k] * m2[k, j];
+                    }
+                }
+            }
+            return resultMatrix;
+        }
+
+        public static Matrix Inverse2x2Matrix(Matrix m)
+        {
+            // see Cryptography and Network Security Principles and Practice, 5th Edition, page 46
+
+            if (!(m.Height == 2 && m.Width == 2))
+                return null; // return nothing if wrong size
+
+            Matrix result = new Matrix(2, 2);
+
+            int det = m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0];
+            int miDet = (int)modInverse(det, 26);
+
+            result[0, 0] = (m[1, 1] * miDet) % 26;
+            result[0, 1] = (-m[0, 1] * miDet) % 26;
+            result[1, 0] = (-m[1, 0] * miDet) % 26;
+            result[1, 1] = (m[0, 0] * miDet) % 26;
+
+            return result;
+        }
+
+        public static Matrix MultipleBy(Matrix m, int n)
+        {            
+            Matrix resultMatrix = new Matrix(m.Height, m.Width);
+
+            for (int i = 0; i < resultMatrix.Height; i++)
+            {
+                for (int j = 0; j < resultMatrix.Width; j++)
+                {
+                    resultMatrix[i, j] = n * m[i, j];
+                }
+            }
+            return resultMatrix;
+        }
+
+        public string String2Show()
+        {
+            string tmp = Format(this[0, 0]) + " " + Format(this[0, 1]) + " " + Format(this[0, 2]) + "\n" + Format(this[1, 0]) + " " + Format(this[1, 1]) + " " + Format(this[1, 2]) + "\n" + Format(this[2, 0]) + " " + Format(this[2, 1]) + " " + Format(this[2, 2]);
+
+            return tmp;
+        }
+
+        private string Format(int n)
+        {
+            return String.Format("{0:0.###############}", n);
+        }
+    }
+}
