e482ddb43c73fc03edbff836bbe88fc747e34363
dreamer2908 <dreamer2908@gmail.com>
2014-09-25 19:23:05 +0700

    Slightly modified the layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -63,14 +63,14 @@
             this.txtPlainText.Location = new System.Drawing.Point(3, 16);
             this.txtPlainText.Multiline = true;
             this.txtPlainText.Name = "txtPlainText";
-            this.txtPlainText.Size = new System.Drawing.Size(430, 89);
+            this.txtPlainText.Size = new System.Drawing.Size(470, 85);
             this.txtPlainText.TabIndex = 0;
             this.txtPlainText.Text = "WEHAVENOTYETDISCUSSEDTWOCOMPLICATIONSTHATEXISTINPICKINGTHEENCRYPTINGMATRIX";
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 175F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 208F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
@@ -82,8 +82,8 @@
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 78F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(442, 306);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(482, 306);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // groupBox2
@@ -91,9 +91,9 @@
             this.tableLayoutPanel1.SetColumnSpan(this.groupBox2, 2);
             this.groupBox2.Controls.Add(this.txtCipherText);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(3, 117);
+            this.groupBox2.Location = new System.Drawing.Point(3, 113);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(436, 108);
+            this.groupBox2.Size = new System.Drawing.Size(476, 104);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Cipher text";
@@ -104,7 +104,7 @@
             this.txtCipherText.Location = new System.Drawing.Point(3, 16);
             this.txtCipherText.Multiline = true;
             this.txtCipherText.Name = "txtCipherText";
-            this.txtCipherText.Size = new System.Drawing.Size(430, 89);
+            this.txtCipherText.Size = new System.Drawing.Size(470, 85);
             this.txtCipherText.TabIndex = 1;
             // 
             // groupBox1
@@ -114,7 +114,7 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(436, 108);
+            this.groupBox1.Size = new System.Drawing.Size(476, 104);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Plain text";
@@ -126,49 +126,53 @@
             this.groupBox3.Controls.Add(this.btnDecrypt);
             this.groupBox3.Controls.Add(this.btnEncrypt);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(3, 231);
+            this.groupBox3.Location = new System.Drawing.Point(3, 223);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(169, 72);
+            this.groupBox3.Size = new System.Drawing.Size(202, 80);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Command";
             // 
             // btnCheckKey
             // 
-            this.btnCheckKey.Location = new System.Drawing.Point(87, 42);
+            this.btnCheckKey.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCheckKey.Location = new System.Drawing.Point(104, 46);
             this.btnCheckKey.Name = "btnCheckKey";
             this.btnCheckKey.Size = new System.Drawing.Size(75, 23);
-            this.btnCheckKey.TabIndex = 3;
+            this.btnCheckKey.TabIndex = 5;
             this.btnCheckKey.Text = "&Check key";
             this.btnCheckKey.UseVisualStyleBackColor = true;
             this.btnCheckKey.Click += new System.EventHandler(this.btnCheckKey_Click);
             // 
             // btnNewKey
             // 
-            this.btnNewKey.Location = new System.Drawing.Point(87, 16);
+            this.btnNewKey.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNewKey.Location = new System.Drawing.Point(104, 20);
             this.btnNewKey.Name = "btnNewKey";
             this.btnNewKey.Size = new System.Drawing.Size(75, 23);
-            this.btnNewKey.TabIndex = 2;
+            this.btnNewKey.TabIndex = 4;
             this.btnNewKey.Text = "&New key";
             this.btnNewKey.UseVisualStyleBackColor = true;
             this.btnNewKey.Click += new System.EventHandler(this.btnNewKey_Click);
             // 
             // btnDecrypt
             // 
-            this.btnDecrypt.Location = new System.Drawing.Point(6, 42);
+            this.btnDecrypt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDecrypt.Location = new System.Drawing.Point(23, 46);
             this.btnDecrypt.Name = "btnDecrypt";
             this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
-            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.TabIndex = 3;
             this.btnDecrypt.Text = "&Decrypt";
             this.btnDecrypt.UseVisualStyleBackColor = true;
             this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
             // 
             // btnEncrypt
             // 
-            this.btnEncrypt.Location = new System.Drawing.Point(6, 16);
+            this.btnEncrypt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEncrypt.Location = new System.Drawing.Point(23, 20);
             this.btnEncrypt.Name = "btnEncrypt";
             this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
-            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.TabIndex = 2;
             this.btnEncrypt.Text = "&Encrypt";
             this.btnEncrypt.UseVisualStyleBackColor = true;
             this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
@@ -183,30 +187,32 @@
             this.groupBox4.Controls.Add(this.numKey01);
             this.groupBox4.Controls.Add(this.numKey00);
             this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox4.Location = new System.Drawing.Point(178, 231);
+            this.groupBox4.Location = new System.Drawing.Point(211, 223);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(261, 72);
+            this.groupBox4.Size = new System.Drawing.Size(268, 80);
             this.groupBox4.TabIndex = 5;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Key";
             // 
             // rbtnKeySelect2
             // 
+            this.rbtnKeySelect2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.rbtnKeySelect2.AutoSize = true;
-            this.rbtnKeySelect2.Location = new System.Drawing.Point(136, 35);
+            this.rbtnKeySelect2.Location = new System.Drawing.Point(153, 36);
             this.rbtnKeySelect2.Name = "rbtnKeySelect2";
             this.rbtnKeySelect2.Size = new System.Drawing.Size(14, 13);
-            this.rbtnKeySelect2.TabIndex = 8;
+            this.rbtnKeySelect2.TabIndex = 12;
             this.rbtnKeySelect2.UseVisualStyleBackColor = true;
             // 
             // rbtnKeySelect
             // 
+            this.rbtnKeySelect.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.rbtnKeySelect.AutoSize = true;
             this.rbtnKeySelect.Checked = true;
-            this.rbtnKeySelect.Location = new System.Drawing.Point(98, 33);
+            this.rbtnKeySelect.Location = new System.Drawing.Point(115, 34);
             this.rbtnKeySelect.Name = "rbtnKeySelect";
             this.rbtnKeySelect.Size = new System.Drawing.Size(34, 17);
-            this.rbtnKeySelect.TabIndex = 7;
+            this.rbtnKeySelect.TabIndex = 11;
             this.rbtnKeySelect.TabStop = true;
             this.rbtnKeySelect.Text = "or";
             this.rbtnKeySelect.UseVisualStyleBackColor = true;
@@ -214,18 +220,20 @@
             // 
             // txtKey
             // 
-            this.txtKey.Location = new System.Drawing.Point(156, 30);
+            this.txtKey.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtKey.Location = new System.Drawing.Point(173, 31);
             this.txtKey.Name = "txtKey";
             this.txtKey.ReadOnly = true;
-            this.txtKey.Size = new System.Drawing.Size(100, 20);
-            this.txtKey.TabIndex = 6;
+            this.txtKey.Size = new System.Drawing.Size(75, 20);
+            this.txtKey.TabIndex = 10;
             this.txtKey.Text = "EHBC";
             this.txtKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtKey.Leave += new System.EventHandler(this.txtKey_Leave);
             // 
             // numKey11
             // 
-            this.numKey11.Location = new System.Drawing.Point(52, 45);
+            this.numKey11.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.numKey11.Location = new System.Drawing.Point(69, 46);
             this.numKey11.Maximum = new decimal(new int[] {
             25,
             0,
@@ -233,7 +241,7 @@
             0});
             this.numKey11.Name = "numKey11";
             this.numKey11.Size = new System.Drawing.Size(40, 20);
-            this.numKey11.TabIndex = 5;
+            this.numKey11.TabIndex = 9;
             this.numKey11.Value = new decimal(new int[] {
             2,
             0,
@@ -243,7 +251,8 @@
             // 
             // numKey10
             // 
-            this.numKey10.Location = new System.Drawing.Point(6, 45);
+            this.numKey10.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.numKey10.Location = new System.Drawing.Point(23, 46);
             this.numKey10.Maximum = new decimal(new int[] {
             25,
             0,
@@ -251,7 +260,7 @@
             0});
             this.numKey10.Name = "numKey10";
             this.numKey10.Size = new System.Drawing.Size(40, 20);
-            this.numKey10.TabIndex = 4;
+            this.numKey10.TabIndex = 8;
             this.numKey10.Value = new decimal(new int[] {
             1,
             0,
@@ -261,7 +270,8 @@
             // 
             // numKey01
             // 
-            this.numKey01.Location = new System.Drawing.Point(52, 19);
+            this.numKey01.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.numKey01.Location = new System.Drawing.Point(69, 20);
             this.numKey01.Maximum = new decimal(new int[] {
             25,
             0,
@@ -269,7 +279,7 @@
             0});
             this.numKey01.Name = "numKey01";
             this.numKey01.Size = new System.Drawing.Size(40, 20);
-            this.numKey01.TabIndex = 3;
+            this.numKey01.TabIndex = 7;
             this.numKey01.Value = new decimal(new int[] {
             7,
             0,
@@ -279,7 +289,8 @@
             // 
             // numKey00
             // 
-            this.numKey00.Location = new System.Drawing.Point(6, 19);
+            this.numKey00.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.numKey00.Location = new System.Drawing.Point(23, 20);
             this.numKey00.Maximum = new decimal(new int[] {
             25,
             0,
@@ -287,7 +298,7 @@
             0});
             this.numKey00.Name = "numKey00";
             this.numKey00.Size = new System.Drawing.Size(40, 20);
-            this.numKey00.TabIndex = 2;
+            this.numKey00.TabIndex = 6;
             this.numKey00.Value = new decimal(new int[] {
             4,
             0,
@@ -299,9 +310,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 306);
+            this.ClientSize = new System.Drawing.Size(482, 306);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.MinimumSize = new System.Drawing.Size(450, 340);
+            this.MinimumSize = new System.Drawing.Size(490, 340);
             this.Name = "Form1";
             this.Text = "Hill Cipher";
             this.tableLayoutPanel1.ResumeLayout(false);
