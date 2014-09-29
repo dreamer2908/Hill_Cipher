769d5221aa29a0a47e78bcccf4d6b8baa359ee81
dreamer2908 <dreamer2908@gmail.com>
2014-09-29 08:09:52 +0700

    Added benchmark

--- Hill_Cipher.Research/Program.cs
+++ Hill_Cipher.Research/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace Hill_Cipher.Research
 {
@@ -9,6 +10,7 @@ namespace Hill_Cipher.Research
         static void Main(string[] args)
         {
             verifyKeyConditionVsDecryptability();
+            benchmarkCipher();
         }
 
         static void verifyKeyConditionVsDecryptability()
@@ -21,6 +23,10 @@ namespace Hill_Cipher.Research
             int conditionMet_notDecryptable = 0;
             int conditionNotMet_decryptable = 0;
             int conditionNotMet_notDecryptable = 0;
+
+            benchmark bm = new benchmark();
+            bm.startBenchmark();
+
             for (int k1 = 0; k1 < 26; k1++)
                 for (int k2 = 0; k2 < 26; k2++)
                     for (int k3 = 0; k3 < 26; k3++)
@@ -51,8 +57,34 @@ namespace Hill_Cipher.Research
             Console.WriteLine("Condition met, not decryptable: " + conditionMet_notDecryptable.ToString());
             Console.WriteLine("Condition didn't meet, decryptable: " + conditionNotMet_decryptable.ToString());
             Console.WriteLine("Condition didn't meet, not decryptable: " + conditionNotMet_notDecryptable.ToString());
-            Console.WriteLine("Total: " + total.ToString() + ". ");
+            Console.WriteLine("Total: " + total.ToString() + " ");
+            Console.WriteLine("Verification took " + bm.getBenchmarkResult() + ".\n");
             
+        }
+
+        static void benchmarkCipher()
+        {
+            string plainText = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            //plainText = Hill_Cipher.HillCipher2x2.plainTextFullSample(); // if you want a bigger sample
+            Matrix key = Hill_Cipher.Matrix.generateNewKey();
+            int loops = 1000000;
+
+            benchmark bm = new benchmark();
+            bm.startBenchmark();
+
+            for (int i = 0; i < loops; i++)
+            {
+                key = Hill_Cipher.Matrix.generateNewKey();
+            }
+            Console.WriteLine("Generating a new key " + loops.ToString() + " times took " + bm.getBenchmarkResult() + ".");
+
+            bm.startBenchmark();
+            for (int i = 0; i < loops; i++)
+            {
+                string cipherText = Hill_Cipher.HillCipher2x2.encryptText(plainText, key);
+                string _plainText = Hill_Cipher.HillCipher2x2.decryptText(cipherText, key);
+            }
+            Console.WriteLine("Encrypting & decrypting the sample " + loops.ToString() + " times took " + bm.getBenchmarkResult() + ".");
         }
     }
 }
