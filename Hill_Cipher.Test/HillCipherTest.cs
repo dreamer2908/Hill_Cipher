8218359fb64352b92858994b32edd352de5816d8
dreamer2908 <dreamer2908@gmail.com>
2014-10-01 08:03:34 +0700

    Test cases for encrypting, decrypting with 3x3 key

--- Hill_Cipher.Test/HillCipherTest.cs
+++ Hill_Cipher.Test/HillCipherTest.cs
@@ -14,7 +14,7 @@ namespace Hill_Cipher.Test
         [TestCase("WEHAVENOTYETDISCUSSEDTWOCOMPLICATIONSTHATEXISTINPICKINGTHEENCRYPTINGMATRIX", "eiovcfqfgvndexqmquwwlaiguyrhuvegktpdptovyzsfptdlchiedlrjapvzdnprktsryklmhj")]
         [TestCase("zxcvbnmasdfghjklqwertyuiop", "piphpqyktrctpobhuchtgvmwvn")]
         [TestCase("aa", "aa")]
-        public void encryptText_ColumnVectorVariousInputs_ChecksThem(string plainText, string expected)
+        public void encryptText_Key2x2ColumnVectorVariousInputs_ChecksThem(string plainText, string expected)
         {
             Matrix key = new Matrix(2, 2);
             key[0, 0] = 2;
@@ -32,7 +32,7 @@ namespace Hill_Cipher.Test
         [TestCase("WEHAVENOTYETDISCUSSEDTWOCOMPLICATIONSTHATEXISTINPICKINGTHEENCRYPTINGMATRIX", "eiovcfqfgvndexqmquwwlaiguyrhuvegktpdptovyzsfptdlchiedlrjapvzdnprktsryklmhj")]
         [TestCase("zxcvbnmasdfghjklqwertyuiop", "piphpqyktrctpobhuchtgvmwvn")]
         [TestCase("aa", "aa")]
-        public void decryptText_ColumnVectorVariousInputs_ChecksThem(string expected, string cipherText)
+        public void decryptText_Key2x2ColumnVectorVariousInputs_ChecksThem(string expected, string cipherText)
         {
             Matrix key = new Matrix(2, 2);
             key[0, 0] = 2;
@@ -45,7 +45,7 @@ namespace Hill_Cipher.Test
         }
 
         [TestCase("hillcipher", "HCRZSSXNSP")] // sample in book, page 48 - 49
-        public void encryptText_RowVectorVariousInputs_ChecksThem(string plainText, string expected)
+        public void encryptText_Key2x2RowVectorVariousInputs_ChecksThem(string plainText, string expected)
         {
             Matrix key = new Matrix(2, 2);
             key[0, 0] = 3;
@@ -58,7 +58,7 @@ namespace Hill_Cipher.Test
         }
 
         [TestCase("hillcipher", "HCRZSSXNSP")] // sample in book, page 48 - 49
-        public void decryptText_RowVectorVariousInputs_ChecksThem(string expected, string cipherText)
+        public void decryptText_Key2x2RowVectorVariousInputs_ChecksThem(string expected, string cipherText)
         {
             Matrix key = new Matrix(2, 2);
             key[0, 0] = 3;
@@ -66,6 +66,80 @@ namespace Hill_Cipher.Test
             key[1, 0] = 8;
             key[1, 1] = 5;
             Hill_Cipher.HillCipher.useRowMsgVector = true;
+            string re = Hill_Cipher.HillCipher.decryptText(cipherText, key);
+            Assert.AreEqual(expected.ToUpper(), re.ToUpper());
+        }
+
+        [TestCase("paymoremoney", "RRLMWBKASPDH", 17, 17, 5, 21, 18, 21, 2, 2, 19)] // sample in book, page 48 - 49
+        public void encryptText_Key3x3RowVectorVariousInputs_ChecksThem(string plainText, string expected, int m1_00, int m1_01, int m1_02, int m1_10, int m1_11, int m1_12, int m1_20, int m1_21, int m1_22)
+        {
+            Matrix key = new Matrix(3, 3);
+            key[0, 0] = m1_00;
+            key[0, 1] = m1_01;
+            key[0, 2] = m1_02;
+            key[1, 0] = m1_10;
+            key[1, 1] = m1_11;
+            key[1, 2] = m1_12;
+            key[2, 0] = m1_20;
+            key[2, 1] = m1_21;
+            key[2, 2] = m1_22;
+            Hill_Cipher.HillCipher.useRowMsgVector = true;
+            string re = Hill_Cipher.HillCipher.encryptText(plainText, key);
+            Assert.AreEqual(expected.ToUpper(), re.ToUpper());
+        }
+
+        [TestCase("RRLMWBKASPDH", "paymoremoney", 17, 17, 5, 21, 18, 21, 2, 2, 19)] // sample in book, page 48 - 49
+        public void decryptText_Key3x3RowVectorVariousInputs_ChecksThem(string cipherText, string expected, int m1_00, int m1_01, int m1_02, int m1_10, int m1_11, int m1_12, int m1_20, int m1_21, int m1_22)
+        {
+            Matrix key = new Matrix(3, 3);
+            key[0, 0] = m1_00;
+            key[0, 1] = m1_01;
+            key[0, 2] = m1_02;
+            key[1, 0] = m1_10;
+            key[1, 1] = m1_11;
+            key[1, 2] = m1_12;
+            key[2, 0] = m1_20;
+            key[2, 1] = m1_21;
+            key[2, 2] = m1_22;
+            Hill_Cipher.HillCipher.useRowMsgVector = true;
+            string re = Hill_Cipher.HillCipher.decryptText(cipherText, key);
+            Assert.AreEqual(expected.ToUpper(), re.ToUpper());
+        }
+
+        [TestCase("ACT", "POH", 6, 24, 1, 13, 16, 10, 20, 17, 15)] // small sample in wikipedia
+        [TestCase("CAT", "FIN", 6, 24, 1, 13, 16, 10, 20, 17, 15)]
+        public void encryptText_Key3x3ColumnVectorVariousInputs_ChecksThem(string plainText, string expected, int m1_00, int m1_01, int m1_02, int m1_10, int m1_11, int m1_12, int m1_20, int m1_21, int m1_22)
+        {
+            Matrix key = new Matrix(3, 3);
+            key[0, 0] = m1_00;
+            key[0, 1] = m1_01;
+            key[0, 2] = m1_02;
+            key[1, 0] = m1_10;
+            key[1, 1] = m1_11;
+            key[1, 2] = m1_12;
+            key[2, 0] = m1_20;
+            key[2, 1] = m1_21;
+            key[2, 2] = m1_22;
+            Hill_Cipher.HillCipher.useRowMsgVector = false;
+            string re = Hill_Cipher.HillCipher.encryptText(plainText, key);
+            Assert.AreEqual(expected.ToUpper(), re.ToUpper());
+        }
+
+        [TestCase("POH", "ACT", 6, 24, 1, 13, 16, 10, 20, 17, 15)] // small sample in wikipedia
+        [TestCase("FIN", "CAT", 6, 24, 1, 13, 16, 10, 20, 17, 15)]
+        public void decryptText_Key3x3ColumnVectorVariousInputs_ChecksThem(string cipherText, string expected, int m1_00, int m1_01, int m1_02, int m1_10, int m1_11, int m1_12, int m1_20, int m1_21, int m1_22)
+        {
+            Matrix key = new Matrix(3, 3);
+            key[0, 0] = m1_00;
+            key[0, 1] = m1_01;
+            key[0, 2] = m1_02;
+            key[1, 0] = m1_10;
+            key[1, 1] = m1_11;
+            key[1, 2] = m1_12;
+            key[2, 0] = m1_20;
+            key[2, 1] = m1_21;
+            key[2, 2] = m1_22;
+            Hill_Cipher.HillCipher.useRowMsgVector = false;
             string re = Hill_Cipher.HillCipher.decryptText(cipherText, key);
             Assert.AreEqual(expected.ToUpper(), re.ToUpper());
         }
